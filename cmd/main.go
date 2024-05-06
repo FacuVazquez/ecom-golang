4fa04711c7b9d6497dc70e7a04bf2d5652149c08
Facundo Vazquez <facundofvazquez@gmail.com>
2024-05-06 20:21:35 -0300

    created config file for db, added godotenv, added env vars

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,26 @@ import (
 	"log"
 
 	"github.com/FacuVazquez/ecom-golang/cmd/api"
+	"github.com/FacuVazquez/ecom-golang/config"
+	"github.com/FacuVazquez/ecom-golang/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
